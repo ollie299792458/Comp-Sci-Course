5aa1c4cc4851f528b0d8369a6836549d4a47053a
Oliver Black <oliver.black.uk@gmail.com>
2016-10-08 11:37:25 +0100

    Fixed tick1 star

--- ML Ticks/tick1star.ml
+++ ML Ticks/tick1star.ml
@@ -1,20 +1,20 @@
-fun eapproxb(n:int, c:int,x:real) = 
+fun eapproxb(n:int, c:int, x:real) : real= 
 	if (n+1 = c) then
 		0.0
 	else
 		x + eapproxb(n, (c+1), (x*(1.0/real(c))));
 
-fun eapprox(n:int) = eapproxb(n, 1, 1.0);
+fun eapprox(n:int) : real = eapproxb(n, 1, 1.0);
+		
+fun expb(n:int, c:int, x:real, z:real) : real = 
+	if (n+1 = c) then
+		0.0
+	else
+		x + expb(n, (c+1), (x*(z/real(c))), z);
 
-fun power(n:int, x:real) = 
-	if (n = 1) then x else 
-	if ((n mod 2) = 0) then 
-		power(n div 2, x * x) else
-		x * power(n div 2, x * x);
+fun exp(z:real, n:int) : real = expb(n, 1, 1.0, z);
 
-fun exp(z:int, n:int) = power(z, eapprox(n));
-
-fun gcd(a:int, b:int) = 
+fun gcd(a:int, b:int) : int = 
 	if (a=b) then a else
 	if (((a mod 2) = 0) andalso ((b mod 2) = 0)) then 2*gcd(a div 2, b div 2) else
 	if (((a mod 2) + (b mod 2)) = 1) then 
