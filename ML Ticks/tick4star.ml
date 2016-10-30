b967c376d0d57f12231ee6736a41039b7db4732c
Oliver Black <oliver.black.uk@gmail.com>
2016-10-30 17:18:05 +0000

    Done tick4 star

--- ML Ticks/tick4star.ml
+++ ML Ticks/tick4star.ml
@@ -2,8 +2,20 @@ datatype 'a stream = Cons of 'a * (unit -> 'a stream);
 fun from k = Cons(k, fn() => from(k+1));
 
 fun merge (Cons(x, xs), Cons(y, ys)) = 
-        if (x < y)
+        if (x = y)
+        then Cons(x, fn() => merge (xs(), ys()))
+        else if (x < y)
         then Cons(x, fn() => merge (xs(), Cons(y, ys)))
         else Cons(y, fn() => merge (Cons(x, xs), ys()));
 
+fun pows23from k = merge(
+            Cons((2*k), fn() => pows23from (2*k)),
+            Cons((3*k), fn() => pows23from (3*k)));
+
+val pows23 = Cons(1,fn() => pows23from 1);
+
+fun pows235from k = merge(pows23from k,
+            Cons((5*k), fn() => pows235from (5*k)));
+
+val pows235 = Cons(1, fn() => pows235from 1);
 
