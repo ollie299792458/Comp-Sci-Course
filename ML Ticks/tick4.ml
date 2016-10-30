249a8649c9548d3a353a4afde068b3d8225ae5f7
Oliver Black <oliver.black.uk@gmail.com>
2016-10-30 13:16:32 +0000

    Done tick 4 not star

--- ML Ticks/tick4.ml
+++ ML Ticks/tick4.ml
@@ -3,13 +3,19 @@ fun nfold f 0 = (fn x => x)
 
 fun sum m n = nfold (fn x => x+1) n m;
 
-fun product m n = nfold (fn x => x+m) n 0;
+fun product m n = nfold (fn x => sum x m) n 0;
 
-fun power m n = nfold (fn x => x*m) n 1;
+fun power m n = nfold (fn x => product x m) n 1;
 
 datatype 'a stream = Cons of 'a * (unit -> 'a stream);
 fun from k = Cons(k, fn() => from(k+1));
 
-fun nth (Cons(x, xf), 0) = x
-    | nth (Cons(x, xf, n) =  
+fun nth (Cons(x, xf), 1) = x
+    | nth (Cons(x, xf), n) = nth(xf(), n-1);
+
+fun getsquaresfrom k = Cons(power k 2, fn() => getsquaresfrom (k+1));
+
+val squares = getsquaresfrom 1;
+
+fun map2 f (Cons(x, xf)) (Cons(y, yf)) = Cons(f x y, fn() => map2 f (xf()) (yf()));
 
