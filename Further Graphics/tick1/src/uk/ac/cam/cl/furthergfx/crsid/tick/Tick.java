00a27190d273cd67ae9eda23076bf16a7a3f43e8
Oliver Black <oliver.black.uk@gmail.com>
2017-10-13 20:19:27 +0100

    Added (finally) further graphics

---
+++ Further Graphics/tick1/src/uk/ac/cam/cl/furthergfx/crsid/tick/Tick.java
@@ -0,0 +1,12 @@
+package uk.ac.cam.cl.furthergfx.crsid.tick;
+
+public class Tick {
+
+  public static void main(String[] args) {
+    TickApp app = new TickApp();
+
+    app.init();
+    app.run();
+    app.shutdown();
+  }
+}
