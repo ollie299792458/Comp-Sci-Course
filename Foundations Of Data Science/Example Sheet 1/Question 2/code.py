557cfaa9bd4e40a177f449c95f27dc97047b941d
Oliver Black <oliver.black.uk@gmail.com>
2017-10-15 15:08:11 +0100

    Attempt (failed) at data science questions

---
+++ Foundations Of Data Science/Example Sheet 1/Question 2/code.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt  # for plotting (MATLAB-style)
+import numpy as np               # for numerical computation (MATLAB-style)
+import scipy.optimize            # for numerical optimization
+import pandas                    # for data import
+import csv
+
+reader = csv.DictReader(open("wasp.csv"), delimiter=',')
+print("Opened File")
+day1 = []
+day2 = []
+day3 = []
+for line in reader:
+    if line["day"] == "1":
+        day1.append(line["t_sec"])
+    elif line["day"] == "2":
+        day2.append(line["t_sec"])
+    elif line["day"] == "3":
+        day3.append(line["t_sec"])
+    else:
+        print("wtf: "+line["day"]+", "+line["t_sec"])
+print("Read file into arrays")
+plt.plot(range(10), day1, "Day 1")
+plt.plot(range(10), day2, "Day 2")
+plt.plot(range(10), day3, "Day 3")
+plt.show
