cf51340592cc4ceeac0de68a7fd0f3eb1d99d729
Oliver Black <oliver.black.uk@gmail.com>
2017-10-17 17:07:13 +0100

    Moved rotary pos around

--- ECAD/exercise1/rotary.sv
+++ ECAD/exercise1/rotary.sv
@@ -11,8 +11,8 @@ module rotary
    );
 
 /* Wire and register definitions */
-logic [7:0] last_pos;
-logic [1:0] clean_rotary;
+reg [7:0] last_pos;
+reg [1:0] clean_rotary;
 
 /* Debouncing components */
 debounce dut_one(.clk(clk), .rst(rst), .bouncy_in(rotary_in[0]), .clean_out(clean_rotary[0]));
@@ -22,7 +22,6 @@ debounce dut_two(.clk(clk), .rst(rst), .bouncy_in(rotary_in[1]), .clean_out(clea
 always_ff @(posedge clk or posedge rst)
 if (rst)
 begin
-    clean_rotary <= 0;
     last_pos <= 0;
 end 
 else
@@ -31,8 +30,10 @@ begin
         if (clean_rotary == 2'b00) begin
             if (last_pos == 2'b01) begin
                 rot_cw <= 1;
+                rotary_pos <= rotary_pos + 1;
             end else if (last_pos == 2'b10) begin
                 rot_ccw <= 1;
+                rotary_pos <= rotary_pos - 1;
             end;
         end;
         if (last_pos == 2'b00) begin
