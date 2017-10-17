5e15c5dd79a98fd0e62eb77a17e487dec1dbda07
Oliver Black <oliver.black.uk@gmail.com>
2017-10-17 14:42:05 +0100

    Moved initial state

--- ECAD/exercise0/tlight.sv
+++ ECAD/exercise0/tlight.sv
@@ -7,8 +7,8 @@ logic [2:0] state;
 
 // enter code here
 always_ff @(posedge clk)
+initial state=0;
 begin
-  initial state=0;
   state = (state == 3'b100) ? 3'b110:
           (state == 3'b110) ? 3'b001:
           (state == 3'b001) ? 3'b010:
