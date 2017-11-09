1eba2d191488a3df696d843f95c6a3e63bfb03af
Oliver Black <oliver.black.uk@gmail.com>
2017-11-09 14:06:22 +0000

    Done rust demo, onto task now

--- Rust-Demo/my-project/src/main.rs
+++ Rust-Demo/my-project/src/main.rs
@@ -1,15 +1,59 @@
+//import
+use std::cell::Cell;
+
+//enumeration
+enum LogMessage {
+    HelloWorld,
+    AString(String),
+}
+
+//interface
 trait Logger {
-    fn log(&self, message: String);
+    //only (immutably) borrowing object, "self" would own it, 
+    //"&mut self" would mutably borrow it
+    fn log(&self, message: LogMessage);
 }
 
+//data
 struct LogPrinter {
-    message_id: u32
+    //Generic type - allow interior mutability, only the place that owns the
+    //cell can free/destroy the cell
+    message_id: Cell<u32>
 }
 
+//implementation
 impl LogPrinter {
     fn new() -> LogPrinter {
         LogPrinter {
-            message_id: 0
+            message_id: Cell::new(0)
         }
     }
+}
+
+//making data and implementation obey trait
+impl Logger for LogPrinter {
+    fn log(&self, message: LogMessage) {
+        //retrieve id as an int from LogPrinter
+        let message_id = self.message_id.get();
+        //println!() a macro, not a method
+        match message {
+            //can match on enums, or actual values, and have defaults
+            //if match uses owned value, ownership moved,
+            //if borrowed value, ownership remains, but...
+            LogMessage::HelloWorld =>
+                println!("{}: Hello World!", message_id),
+            LogMessage::AString(string) =>
+                println!("{}: {}", message_id, string),
+        }
+        //return id to LogPrinter
+        self.message_id.set(message_id + 1);
+    }
+}
+
+fn main() {
+    //logger:LogPrinter - Type inferance
+    let logger = LogPrinter::new();
+    logger.log(LogMessage::HelloWorld);
+    //"Rust = C + ML" is a statically allocated immutable string
+    logger.log(LogMessage::AString("Rust = C + ML".into()));
 }
---
+++ Rust-Demo/my-project/target/debug/my-project.d
@@ -0,0 +1,1 @@
+/home/oliver/Documents/Git\ Reps/Comp-Sci-Course/Rust-Demo/my-project/target/debug/my-project: /home/oliver/Documents/Git\ Reps/Comp-Sci-Course/Rust-Demo/my-project/src/main.rs
