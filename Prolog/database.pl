83fd4ba1ee4281862d885385698786219430f58d
Oliver Black <oliver.black.uk@gmail.com>
2017-11-25 17:35:06 +0000

    Done first part of prolog database questions

---
+++ Prolog/database.pl
@@ -0,0 +1,16 @@
+tName(acr31,'Andrew Rice').
+tName(arb33,'Alastair Beresford').
+tName(olb22,'Oliver Black')
+
+tCollege(acr31, 'Churchill').
+% tCollege(arb33, 'Robinson').
+tCollege(olb22, 'Selwyn')
+
+tGrade(acr31,’IA’,2.1).
+tGrade(acr31,’IB’,1).
+tGrade(acr31,’II’,1).
+tGrade(arb33,’IA’,2.1).
+tGrade(arb33,’IB’,1).
+tGrade(arb33,’II’,1).
+
+tDOB(olb22, 19970407).
