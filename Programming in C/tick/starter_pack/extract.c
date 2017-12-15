f71fb00927025f0019d33c03614dfa26c9555323
Oliver Black <oliver.black.uk@gmail.com>
2017-12-15 08:32:53 +0800

    In theory finished task3

--- Programming in C/tick/starter_pack/extract.c
+++ Programming in C/tick/starter_pack/extract.c
@@ -1,75 +1,7 @@
 /* extract.c */
 #include <stdio.h>
-#include <string.h>
 
-#define BUFSIZE 1048576
-#define BUFSIZESTRING "BUFSIZE"
-
-/*Removes IP headers - returns total number of bytes removed, or -1 on failure*/
-int remove_iptcp (FILE *fi, FILE *fo) {
-    //declare buffers
-    unsigned char bufi[BUFSIZE];
-    unsigned char bufo[BUFSIZE];
-    int loci = 0;
-    int loco = 0;
-    
-    //read file
-    int r = fread(bufi,1,BUFSIZE,fi);
-    if (r > BUFSIZE) {
-        return -1;
-    }
-    
-    while (loci <= r) {
-        //get the IHL, second 4 bits of first byte
-        int ihl = bufi[loci] & 0x0F;
-        loci += 1;
-       
-        //ignore the second byte
-        loci += 1;
-
-        //get the total length
-        int tl = (bufi[loci]<<8) + bufi[loci+1];
-        loci += 2;
-        
-        //if length is 0 break
-        if (tl == 0) {
-            break;
-        }
-        
-        //go back to start of ip header
-        loci -= 4;
-        
-        //skip to end of the ip header
-        loci += 4*(ihl);
-        
-        //get bytes in tcp packet
-        int btcp = tl-ihl*4;
-        
-        //skip first 12 tcp bytes
-        loci += 12;
-      
-        //get the TCP packet's header size
-        int dof = bufi[loci] >> 4;
-        
-        //go back to start of tcp header
-        loci -= 12;
-        
-        //skip to end of TCP header
-        loci += 4*(dof);
-        
-        //get bytes to copy
-        int btc = btcp-(dof*4);
-        
-        //copy bytes
-        memcpy(&bufi[loci], &bufo[loco], btc);
-        
-        //update locations
-        loci += btc;
-        loco += btc;
-    }
-    int w = fwrite(bufo,1, loco, fo);
-    return r-w;
-}
+#define HEADERBUFFER 4
 
 int main(int argc, char *argv[]) {
     FILE *fm, *fo;
@@ -91,11 +23,47 @@ int main(int argc, char *argv[]) {
         return 3;
     }
     
-    //remove headers
-    if (remove_iptcp(fm, fo)==-1){
-        perror("Failed to remove IP/TCP headers, check your file is under "BUFSIZESTRING" bytes");
-        return 4;
+    unsigned char buf[HEADERBUFFER];
+    int r = 1;
+    
+    while (r > 0) {
+        //read useful ip header information
+        r = fread(buf, 1, HEADERBUFFER, fm);
+        
+        //get header length and total length
+        int ihl = buf[0] & 0x0F;
+        int tl = (buf[2] << 8) + buf[3];
+        
+        //move to end of ip header
+        int mv = ihl * 4 - HEADERBUFFER;
+        fseek(fm, mv, SEEK_CUR);
+        
+        //skip first 12 bytes of tcp header
+        fseek(fm, 12, SEEK_CUR);
+        
+        //read useful tcp header information
+        fread(buf, 1, HEADERBUFFER, fm);
+        
+        //get data offset (tcp header size)
+        int doff = buf[0] >> 4;
+        
+        //move to end of tcp header
+        mv = doff * 4 - 12 - HEADERBUFFER;
+        fseek(fm, mv, SEEK_CUR);
+        
+        //calculate data length
+        int dl = tl - ihl * 4 - doff * 4;
+        
+        //copy data to output file
+        unsigned char dbuf[dl];
+        fread(dbuf, 1, dl, fm);
+        fwrite(dbuf, 1, dl, fo);
     }
+    
+    
+    //close files
+    fclose(fm);
+    fclose(fo);
     
     return 0;
 }
